d6f587a1ec111ebcb232d3172db01cbc6d3fbbef
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-12 23:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function removeArticales(name) {
+	return name.replace(/^(a|an|the)\s+/i,' ');
+}
+
+touristSpots.sort((a,b)=> removeArticales(a).localeCompaire(removeArticales(b)));
+
+let ul =document.getElementsByTagName("ul")[0];
+ul.id ="bands"
+
+touristSpots.forEach((touristSpot)=>
+	let li = document.createElement("li")
+	li.textContent = touristSpot;
+	ul.appendChild(li)
+					
+)
